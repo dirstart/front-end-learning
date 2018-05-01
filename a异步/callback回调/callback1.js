707d8b2ee07a7ed02b38195229c33264485e60a7
zhuqianye <q835041058@gmail.com>
2018-05-01 23:43:20 +0800

    callback

---
+++ a异步/callback回调/callback1.js
@@ -0,0 +1,8 @@
+// 刀塔2 选英雄画面之 对手先选英雄，我再选英雄。
+const advantageAsync = (callback) => {
+  setTimeout(() => {
+    for (i = 0; i < 1000000000; i++) {};
+  }, 0);
+  console.log('choose a');
+}
+advantageAsync();
